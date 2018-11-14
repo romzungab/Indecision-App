0360f925bb20c2737713ceddf88f6b0ede4b2a9b
Roms <romelyn.ungab@ipfx.com>
2018-11-15 12:02:06 +1300

    added src

---
+++ public/src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is runing');
+
+//JSX Javascript XML
+
+var template = <p>This is jsx from app.js!</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
