50fe2c8cca93da08bd2f8557c890f5af6da7494f
Roms <romelyn.ungab@ipfx.com>
2018-11-15 14:39:46 +1300

    simple interaction

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,56 +1,104 @@
 'use strict';
 
-var add = function add(a, b) {
-    //console.log(arguments);
-    return a + b;
+console.log('App.js is runing');
+
+//JSX Javascript XML
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: [] //['One','Two']
 };
-
-console.log(add(55, 1, 1001));
-
-// const user = {
-//     name:'Roms',
-//     cities : ['Iligan', 'Auckland', 'Manila'],
-//     printPlacesLived : function (){
-//           this.cities.forEach( (city) =>{
-//             console.log(this.name +' has lived in '+ city)
-//         })
-//     }
-// };
-//no more function
-// const user ={
-//     name : 'Roms',
-//     cities: ['Iligan', 'Auckland', 'Manila'],
-//     printPlacesLived(){
-//         this.cities.forEach( (city) =>
-//         { console.log(this.name + ' has lived in ' + city)}
-//         );
-//     }
-// };
-//using map
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No option'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item two'
+        )
+    )
+);
 var user = {
     name: 'Roms',
-    cities: ['Iligan', 'Auckland', 'Manila'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        var cityMessages = this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-        return cityMessages;
-    }
+    age: 36,
+    location: 'Auckland'
 };
 
-console.log(user.printPlacesLived());
+var userName = 'Romelyn';
+var userAge = 26;
+var userLocation = 'Auckland';
 
-var multiplier = {
-    multiplicand: [10, 20, 30],
-    multiplyBy: 2,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.multiplicand.map(function (mul) {
-            return mul * _this2.multiplyBy;
-        });
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
     }
+}
+var count = 0;
+var addOne = function addOne() {
+    console.log('addOne');
 };
-console.log(multiplier.multiply());
+
+var minusOne = function minusOne() {
+    console.log('minusOne');
+};
+
+var reset = function reset() {
+    console.log('reset');
+};
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count:',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: reset },
+        'reset'
+    )
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
