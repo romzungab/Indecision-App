e82ad5729f5f86103cefa7c32d84efeeb760d6cd
Roms <romelyn.ungab@ipfx.com>
2018-11-19 13:47:35 +1300

    importing react and reactdom

--- src/app.js
+++ src/app.js
@@ -1,13 +1,5 @@
-// import './utils.js';
-// import substract, { add, square } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-//  console.log('app.js is running hello');
-//  console.log(square(4));
-//  console.log(add(4, 5));
-// console.log(substract(100, 81))
-
- import isSenior, { canDrink, isAdult } from './person.js';
-
- console.log(canDrink(20));
- console.log(isAdult(20));
-console.log(isSenior(65));
+const template = React.createElement('p',{}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
