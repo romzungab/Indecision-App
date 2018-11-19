3c187a5c13bf791fe9b94032fdd583cd21f948f7
Roms <romelyn.ungab@ipfx.com>
2018-11-19 15:21:49 +1300

    class properties syntax

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,16 +5,37 @@ import Header from './Header';
 import Options from './Options';
 
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options: []
-        };
+    state = {
+        options : []
     }
+
+    handleDeleteOptions = () => {
+        this.setState(() => ({ options: [] }));
+    }
+
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => option !== optionToRemove)
+        }));
+    }
+
+    handlePick = () => {
+        const random = Math.floor(Math.random() * this.state.options.length);
+        const option = this.state.options[random];
+        alert(option);
+    }
+
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if (this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
+        }
+        this.setState((prevState) => ({
+            options: prevState.options.concat([option])
+        }));
+    }
+    
     //lifecycle methods
     componentDidMount() { // fetching data
         try {
@@ -34,37 +55,10 @@ export default class IndecisionApp extends React.Component {
         }
     }
 
-    componentWillUnmount() {
+    componentWillUnmount(){
         console.log('componentWillUnmount');
     }
-
-    handleDeleteOptions() {
-        this.setState(() => ({ options: [] }));
-    }
-
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => option !== optionToRemove)
-        }));
-    }
-
-    handlePick() {
-        const random = Math.floor(Math.random() * this.state.options.length);
-        const option = this.state.options[random];
-        alert(option);
-    }
-
-    handleAddOption(option) {
-        if (!option) {
-            return 'Enter valid value to add item';
-        } else if (this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        }
-        this.setState((prevState) => ({
-            options: prevState.options.concat([option])
-        }));
-
-    }
+    
     render() {
         const subtitle = 'Put your life in the hands of a computer';
         return (
--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,12 @@ module.exports = {
             test:/\.js$/,
             exclude:/node_modules/
         }]
-    } 
+    } ,
+    devtool: 'cheap-module-eval-source-map',
+    devServer:{
+        contentBase:path.join(__dirname, 'public'),
+    },
 };
 
-//loader
+
 
